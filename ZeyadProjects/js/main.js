0b588855d638a81b60fa85522a0e05b873d67b35
Zeyad <zeyad1817.2003@gmail.com>
2026-02-08 22:03:54 +0300

    Number is Done

---
+++ ZeyadProjects/js/main.js
@@ -0,0 +1,10 @@
+document.write("<h1>Hello <span>Page</span></h1>")
+document.write("<h1>Hello <span>Page</span></h1>")
+
+let a = "zeyad"
+let b = "corvfd"
+
+console.log(a,b);
+
+let markUp =`snjskn`
+'<div class= "parent">'
