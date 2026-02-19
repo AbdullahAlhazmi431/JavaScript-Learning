a4f4d9d978db8753b34d8d1c1f205ff171cedc16
Zeyad <zeyad1817.2003@gmail.com>
2026-02-19 04:56:24 +0300

    Function And Scop is Done

--- ZeyadProjects/js/Function.js
+++ ZeyadProjects/js/Function.js
@@ -140,3 +140,78 @@ function multiply(a, b, c) {
 console.log(multiply(10, 20)); // 200
 console.log(multiply("A", 10, 30)); // 300
 console.log(multiply(100.5, 10, "B")); // 1000
+
+let myNumbers = [20, 50, 10, 60];
+let calc = (one, two, ...nums) => one + two + nums[0];
+
+console.log(calc(10, myNumbers.pop(), myNumbers.pop())); // 80
+
+// A1
+function getDetails(zName, zAge, zCountry) {
+  function namePattern(zName) {
+    let parts = zName.split(" ");
+    let firstName = parts[0];
+    let lastInitial = parts[1] ? parts[1][0].toUpperCase() + "." : "";
+    return `${firstName} ${lastInitial}`;
+  }
+
+  function ageWithMessage(zAge) {
+    let age = zAge.split(" ");
+    age.pop();
+    age.pop();
+    age.pop();
+    return `Your Age Is ${age}`;
+  }
+
+  function countryTwoLetters(zCountry) {
+    return `You Live In ${zCountry.slice(0, 2).toUpperCase()}`;
+  }
+
+  function fullDetails() {
+    return `Hello ${namePattern(zName)}, ${ageWithMessage(zAge)}, ${countryTwoLetters(zCountry)}`;
+  }
+
+  return fullDetails(); // Do Not Edit This
+}
+
+console.log(getDetails("Osama Mohamed", "38 Is My Age", "Egypt"));
+// Hello Osama M., Your Age Is 38, You Live In EG
+
+console.log(getDetails("Ahmed ali", "32 Is The Age", "Syria"));
+// Hello Ahmed A., Your Age Is 32, You Live In SY
+
+// A2
+let itsMeArrow = () => `Iam A Normal Function`;
+
+console.log(itsMeArrow()); // Iam A Normal Function
+
+let urlCreateArrow = (protocol, web, tld) => `${protocol}://www.${web}.${tld}`;
+
+console.log(urlCreateArrow("https", "elzero", "org")); // https://www.elzero.org
+
+// A3
+let checkerArrow = (zName) => (status) => (salary) =>
+  status === "Available"
+    ? `${zName}, My Salary Is ${salary}`
+    : `Iam Not Avaialble`;
+
+console.log(checkerArrow("Osama")("Available")(4000)); // Osama, My Salary Is 4000
+console.log(checkerArrow("Ahmed")("Not Available")()); // Iam Not Avaialble
+
+// A4
+function specialMix(...data) {
+  let sum = 0;
+  for (let i = 0; i < data.length; i++) {
+    let num = parseInt(data[i]);
+    if (!isNaN(num)) {
+      sum += num;
+    }
+  }
+  return sum === 0 ? "All Is Strings" : sum;
+}
+
+// التجربة
+console.log(specialMix(10, 20, 30)); // 60
+console.log(specialMix("10Test", "Testing", "20Cool")); // 30
+console.log(specialMix("Testing", "10Testing", "40Cool")); // 50
+console.log(specialMix("Test", "Cool", "Test")); // All Is Strings
